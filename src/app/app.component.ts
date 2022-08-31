61805abcd22a74d9a2e53c4198130b5ca41f3475
Jari Moellenbernd <jari.moellenbernd@assecosol.com>
2022-08-31 14:35:25 +0200

    chore: provide example of fox-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fox-button-error';
+
+  formGroup: FormGroup;
+
+  submittedValue: { value: string } | null = null;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.formGroup = this._formBuilder.group({
+      value: '',
+    });
+  }
+
+  onSubmit() {
+    this.submittedValue = this.formGroup.value;
+    this.formGroup.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
